a27b00d301d231339e6b6248dd3b98d8cd175e7a
wildanvin <danvin.jim@gmail.com>
2022-04-06 00:27:05 -0500

    Clean fetch users function

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -2,7 +2,6 @@ import spinner from "./assets/spinner.gif"
 function Spinner() {
   return (
     <div className="w-100 mt-20">
-      Spinner
       <img
         width={180}
         className="text-center mx-auto"
