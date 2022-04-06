a619ac1d4e818da5d229594947b4d3eec8ce911a
wildanvin <danvin.jim@gmail.com>
2022-04-06 01:20:52 -0500

    UserSearch Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import UserResults from "../components/users/UserResults"
+import UserSerch from "../components/users/UserSerch"
 
 function Home() {
   return (
     <>
-      {/* SEARCH COMPONENT */}
+      <UserSerch></UserSerch>
       <UserResults></UserResults>
     </>
   )
