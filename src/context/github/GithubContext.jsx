162df0879c9ad87e1482f715f5362359cd65d152
wildanvin <danvin.jim@gmail.com>
2022-04-11 23:13:55 -0500

    Get User Repos

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   }
 
@@ -59,6 +60,32 @@ export const GithubProvider = ({ children }) => {
     }
   }
 
+  //Get user repos
+  const getUserRepos = async (login) => {
+    setLoading()
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    })
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    )
+
+    const data = await response.json()
+
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    })
+  }
+
   //Clear search results
   const clearUsers = () => dispatch({ type: "CLEAR_USERS" })
 
@@ -70,9 +97,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {children}
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -13,6 +13,13 @@ const githubReducer = (state, action) => {
         user: action.payload,
         loading: false,
       }
+
+    case "GET_REPOS":
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false,
+      }
     case "SET_LOADING":
       return {
         ...state,
